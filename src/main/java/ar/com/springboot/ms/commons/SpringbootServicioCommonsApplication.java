5d7698a14c03442ac5da287a67752298ce6e8359
Adrián Canitano <acanitano@gmail.com>
2021-06-29 15:30:09 -0300

    Creando el proyecto en GitHub

---
+++ src/main/java/ar/com/springboot/ms/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,13 @@
+package ar.com.springboot.ms.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioCommonsApplication.class, args);
+	}
+
+}
